72a3073b9d4d4d05a0efa8d2ba623d8fc49d26a9
Jojo1178 <cassenjonathan@gmail.com>
2015-04-30 01:02:50 +0200

    UI Base
    - Show wood and food stocks
    
    Signed-off-by: Jojo1178 <cassenjonathan@gmail.com>

---
+++ GeneticAlgorithm/Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    public BaseStacksManager Stacks;
+
+    public Text WoodStock;
+    public Text FoodStock;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        WoodStock.text = "Wood : " + Stacks.ResourcesStacked[BaseStacksManager.RESOURCES_TYPE.WOOD];
+        FoodStock.text = "Food : " + Stacks.ResourcesStacked[BaseStacksManager.RESOURCES_TYPE.FOOD];
+	}
+}

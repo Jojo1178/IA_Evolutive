47fd53413604e10769700f11018a3117e1538fee
Jojo1178 <cassenjonathan@gmail.com>
2015-06-24 20:57:09 +0200

    IAManager test and Clear git
    Signed-off-by: Jojo1178 <cassenjonathan@gmail.com>

---
+++ GeneticAlgorithm/Assets/Scripts/IAManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAManager : MonoBehaviour {
+
+    public AI LearningAI;
+    public World CurrentWorld;
+
+	// Use this for initialization
+	void Start () {
+
+        Objectives objs = new Objectives();
+        Types t = new Types();
+        Actions act = new Actions();
+        t.Create(1,"GetWood");
+        act.Create(1,"Drop Wood");
+        objs.Create(true,t,10,act);
+
+        LearningAI.Create(objs, 3, CurrentWorld);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
